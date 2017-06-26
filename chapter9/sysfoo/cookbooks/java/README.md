f85b41713e9f8b0c15a5fa08827eb14bb68e71b1
root <root@ws.codespaces.io>
2017-06-26 06:00:08 +0000

    chapter9 deploy

---
+++ chapter9/sysfoo/cookbooks/java/README.md
@@ -0,0 +1,4 @@
+# java
+
+TODO: Enter the cookbook description here.
+
---
+++ chapter9/sysfoo/cookbooks/tomcat/metadata.rb
@@ -0,0 +1,16 @@
+name 'tomcat'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures tomcat'
+long_description 'Installs/Configures tomcat'
+version '0.1.0'
+depends 'java'
+
+# If you upload to Supermarket you should set this so your cookbook
+# gets a `View Issues` link
+# issues_url 'https://github.com/<insert_org_here>/tomcat/issues' if respond_to?(:issues_url)
+
+# If you upload to Supermarket you should set this so your cookbook
+# gets a `View Source` link
+# source_url 'https://github.com/<insert_org_here>/tomcat' if respond_to?(:source_url)

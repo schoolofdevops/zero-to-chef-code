64ec7ad890fbd4b422e6169bd8754b950409bce2
root <root@ws.codespaces.io>
2017-06-26 04:33:09 +0000

    chapter7

---
+++ chapter7/sysfoo/cookbooks/chef-client/test/integration/service_runit/serverspec/service_runit_spec.rb
@@ -0,0 +1,11 @@
+require 'serverspec'
+
+set :backend, :exec
+
+describe command('ps aux | grep che[f]') do
+  its(:stdout) { should match /chef-client/ }
+end
+
+describe file('/etc/service/chef-client/run') do
+  it { should be_file }
+end
